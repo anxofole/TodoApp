dd670ab2c151e81106cfbf574eeb426cb786ecce
Anxo Fole <afole@plainconcepts.com>
2019-10-03 04:41:58 +0200

    Added DNI directive validator

---
+++ app/directives/dniValidator.directive.js
@@ -0,0 +1,39 @@
+(function () {
+    'use strict';
+
+    angular.module('todoApp').directive('dniValidator', [dniValidator]);
+
+    function dniValidator() {
+
+        // Usage:
+        // <input ng-model="value1" validate-equals="value2" />
+
+        var directive = {
+            restrict: 'A',
+            require: 'ngModel',
+            link: link
+        };
+
+        return directive;
+
+        function link(scope, element, attrs, ngModel) {
+
+            var dniPattern = /^\d{8}[a-zA-Z]$/;
+
+            ngModel.$validators.odd = function (value) {
+                return value.match(dniPattern);
+            };
+            // scope.$watch(attrs.ngModel,
+            //     function( newValue, oldValue ) {
+            //        if(newValue.match(dniPattern)) {
+            //         ngModel.$setValidity('dni', true)
+            //        } else {
+            //         ngModel.$setValidity('dni', false)
+            //        }
+            //     }
+            // );
+
+        }
+    }
+
+})();
